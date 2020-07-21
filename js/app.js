953a56bc6f7ca75ff7e6c8eb87002307fc4093f4
Duha-Saleh <duhamahmoud096@gmail.com>
2020-07-21 12:06:03 +0300

    22

--- js/app.js
+++ js/app.js
@@ -13,44 +13,44 @@ function Img(image_url, title, description, keyword, horns){
     imgs.push(this);
 };
 
-$.ajax('./data/page-1.json')
-.then(data =>{
-    data.forEach(value =>{
-        new Img(value.image_url, value.title, value.description, value.keyword, value.horns);
-    })
+// $.ajax('./data/page-1.json')
+// .then(data =>{
+//     data.forEach(value =>{
+//         new Img(value.image_url, value.title, value.description, value.keyword, value.horns);
+//     })
     
-    imgs.forEach(img =>{
-        let photos = $('section');
-        let clonePhotos = photos.clone();
-        clonePhotos.attr('id', `${img.keyword}`);
-        clonePhotos.attr('class', 'info');
-        clonePhotos.children('h2').text(img.title);
-        clonePhotos.children('img').attr('src', `${img.url}`);
-        clonePhotos.children('p').text(img.description);
-        $('main').append(clonePhotos[0]);
-        if(!keywords.includes(img.keyword)) {
-            keywords.push(img.keyword);
-        }
-    });
+//     imgs.forEach(img =>{
+//         let photos = $('section');
+//         let clonePhotos = photos.clone();
+//         clonePhotos.attr('id', `${img.keyword}`);
+//         clonePhotos.attr('class', 'info');
+//         clonePhotos.children('h2').text(img.title);
+//         clonePhotos.children('img').attr('src', `${img.url}`);
+//         clonePhotos.children('p').text(img.description);
+//         $('main').append(clonePhotos[0]);
+//         if(!keywords.includes(img.keyword)) {
+//             keywords.push(img.keyword);
+//         }
+//     });
     
-    keywords.forEach(keyword =>{
-        let options = $('option');
-        let cloneOptions = options.clone();
-        cloneOptions.attr('value', keyword);
-        cloneOptions.text(keyword);
-        $('select').append(cloneOptions[0]);
-    });
+//     keywords.forEach(keyword =>{
+//         let options = $('option');
+//         let cloneOptions = options.clone();
+//         cloneOptions.attr('value', keyword);
+//         cloneOptions.text(keyword);
+//         $('select').append(cloneOptions[0]);
+//     });
     
-    $('select').change(event =>{
-        $('section').each(function(){
-            $(this).show();
-            if(event.target.value == 'default'){
-                $(this).show();
-            }else if($(this).attr('id') !== event.target.value){
-                $(this).hide();
-            }if($(this).attr('class') === event.target.value){
-                $(this).toggle();
-            }
-        });
-    });
-});
+//     $('select').change(event =>{
+//         $('section').each(function(){
+//             $(this).show();
+//             if(event.target.value == 'default'){
+//                 $(this).show();
+//             }else if($(this).attr('id') !== event.target.value){
+//                 $(this).hide();
+//             }if($(this).attr('class') === event.target.value){
+//                 $(this).toggle();
+//             }
+//         });
+//     });
+// });
